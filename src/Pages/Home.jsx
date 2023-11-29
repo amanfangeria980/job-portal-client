18aa3619e66aed9cdac422237afe831a5ec49cd0
amanfangeria980 <amanfangeria980@gmail.com>
2023-11-29 22:43:25 +0530

    ---

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,67 +1,85 @@
-import React,{useEffect, useState} from 'react'
-import Banner from '../components/Banner'
-import Card from './../components/Card';
-import Jobs from './Jobs';
+import React, { useEffect, useState } from "react";
+import Banner from "../components/Banner";
+import Card from "./../components/Card";
+import Jobs from "./Jobs";
 const Home = () => {
-  const [query,setQuery]=useState("");
-  const handleInputChange=(e)=>{
-      setQuery(e.target.value);
-  }
-  const [selectedCategory,setSelectedCategory]=useState(null);
-  const [jobs,setJobs]=useState([]);
-  useEffect(()=>{
-    fetch("jobs.json").then(res=>res.json()).then(data=>setJobs(data));
-  },[])
+  const [query, setQuery] = useState("");
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
 
   // Filter jobs by title
-  const filteredItems=jobs.filter((job)=>job.jobTitle.toLowerCase().indexOf(query.toLowerCase())!==-1);
+  const filteredItems = jobs.filter(
+    (job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
   // console.log(filteredItems)
 
   // Radio Filtering
-  const handleChange=(e)=>{
+  const handleChange = (e) => {
     setSelectedCategory(e.target.value);
-  }
+  };
   // Button based Filtering
-  const handleClick=(e)=>{
+  const handleClick = (e) => {
     setSelectedCategory(e.target.value);
-  }
+  };
   // Main function
-  const filteredData=(jobs,selected,query)=>{
-    let filteredJobs=jobs;
+  const filteredData = (jobs, selected, query) => {
+    let filteredJobs = jobs;
     // filter input items
-    if(query){
-      filteredJobs=filteredItems;
+    if (query) {
+      filteredJobs = filteredItems;
     }
 
     // category filtering
-    if(selected){
-      filteredJobs=filteredJobs.filter(({jobLocation,maxPrice,experienceLevel,salaryType,employmentType,postingDate})=>{
-        jobLocation.toLowerCase()===selected.toLowerCase() || parseInt(maxPrice)<=parseInt(selected) ||
-        salaryType.toLowerCase()==selected.toLowerCase() || employmentType.toLowerCase()===selected.toLowerCase();
+    if (selected) {
+      filteredJobs = filteredJobs.filter(
+        ({
+          jobLocation,
+          maxPrice,
+          experienceLevel,
+          salaryType,
+          employmentType,
+          postingDate,
+        }) => {
+          jobLocation.toLowerCase() === selected.toLowerCase() ||
+            parseInt(maxPrice) <= parseInt(selected) ||
+            salaryType.toLowerCase() == selected.toLowerCase() ||
+            employmentType.toLowerCase() === selected.toLowerCase();
 
-        console.log(filteredJobs);
-      })
+          console.log(filteredJobs);
+        }
+      );
     }
-    return filteredJobs.map((data,i)=>
-      <Card data={data} key={i}/>
-    )
-  }
+    return filteredJobs.map((data, i) => <Card data={data} key={i} />);
+  };
 
-  const result=filteredData(jobs,selectedCategory,query);
+  const result = filteredData(jobs, selectedCategory, query);
 
   return (
     <div>
-      <Banner query={query} handleInputChange={handleInputChange}/>
+      <Banner query={query} handleInputChange={handleInputChange} />
 
       {/* Main Content */}
 
-      <div className='bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12'>
-        <div className='bg-white p-4 rounded'>Left </div>
-        <Jobs result={result} className="bg-white p-4 rounded-sm"/>
-        <div className='bg-white p-4 rounded'>Right</div>
+      <div className="bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12">
+        {/* Left side */}
+        <div className="bg-white p-4 rounded">Left </div>
+
+        {/* Job cards */}
+        <Jobs result={result} className="bg-white p-4 rounded-sm" />
+
+        {/* Right Side */}
+        <div className="bg-white p-4 rounded">Right</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
